1614f6c8ef17d7016f398dea3c7352469c573379
roman883 <s.rb@list.ru>
2020-03-29 15:46:16 +0900

    Initial commit

---
+++ chat-client/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module ru.list.rb.s {
+    requires javafx.controls;
+    exports ru.list.rb.s;
+}
---
+++ chat-client/src/main/java/ru/list/rb/s/SystemInfo.java
@@ -0,0 +1,13 @@
+package ru.list.rb.s;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
